96e82e9da8e40508ce50ddcc0c978be8e5d5b317
drew <nooooru@gmail.com>
2020-04-15 21:58:22 +0800

    fix script

--- packages/server/src/services/background.ts
+++ packages/server/src/services/background.ts
@@ -1,8 +1,7 @@
 import MQClient, { QUEUE } from '../clients/mq'
-import { Location, LocationMeta } from '../models/location'
+import { LocationMeta } from '../models/location'
 import { dateStr } from '../utils'
 import fs from 'fs'
-import PersonaleModule from '../modules/personale'
 
 class LocationLogGenerator {
   static Limit = 100
@@ -106,27 +105,11 @@ const LocationLineFormatter = (
   return `${terminalId};${vehicleId};${personId};${latitude};${longitude};${altitude};${datetime};${area};${terminalStatus};${personStatus};`
 }
 
-setTimeout(async () => {
-  // PersonaleModule.upsertUser({
-  //   Id: '123123',
-  //   IMSI: '41234124123',
-  // })
-  let result = await PersonaleModule.getById('123123')
-  console.log('test mongo', result)
-}, 3000)
-;`46,0114013436303131333031333936303533371A5E8EBB7501589FAE06CA2D460000000020000C00070007000053C2
-46,0114013436303131333033363633373234361A5E92D5FA01589FBB06CA2D57000000004C004D001B001B00003B03
-46,0114013436303131333033363633373234361A5E92D62201589FE806CA2D5D000000FF03004C001B001B00003E17
-46,0114013436303131333033363633373234361A5E92D65B01589F2F06CA2D61000000000E004A001B001B00003FA6
-46,0114013436303131333033363633373234361A5E92D69D01589E9D06CA2D53020000000E001D001D001D00003F20
-46,0114013436303131333033363633373234361A5E92D6D301589DE906CA2E5600000000700047001B001B00003E2A
-46,0114013436303131333033363633373234361A5E92D6FF01589F6206CA2E0700000000A30046001B001B00003FB5
-46,0114013436303131333033363633373234361A5E92D75C01589F7906CA2CA4000000000A0038000B000B00003EFD`
-  .split('\n')
-  .forEach((str) => {
-    let location = new Location(str)
-    console.log(location.isValid)
-    if (location.isValid) {
-      MQClient.publish({ queue: { name: QUEUE.GPS_UPLOAD } }, location.toJson())
-    }
-  })
+// setTimeout(async () => {
+//   // PersonaleModule.upsertUser({
+//   //   Id: '123123',
+//   //   IMSI: '41234124123',
+//   // })
+//   let result = await PersonaleModule.getById('123123')
+//   console.log('test mongo', result)
+// }, 3000)
--- packages/server/src/services/coap.ts
+++ packages/server/src/services/coap.ts
@@ -8,12 +8,13 @@ export const QUEUE_GPS_UPLOAD = 'GPS_UPLOAD'
 
 Server.on('request', function (req, res) {
   let { url, payload } = req
+  payload = payload.toString()
   console.info('Url', url)
   console.info('Payload', payload)
   let location = new Location(payload)
   if (location.isValid) {
     MQClient.publish({ queue: { name: QUEUE.GPS_UPLOAD } }, location.toJson())
-    res.end('OK')
+    res.end(Uint8Array.from([170, 170, 0, 0])) // OK code as requirement
   } else {
     res.end('Invalid payload')
   }
