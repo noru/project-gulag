653f9a11f09dceea2bad4ba3badbec0074262332
noru <nooooru@gmail.com>
2020-04-23 17:27:27 +0800

    rewrite mq connection func

--- packages/server/src/clients/mq.ts
+++ packages/server/src/clients/mq.ts
@@ -1,70 +1,56 @@
-import { connect, Connection, Channel } from 'amqplib'
+import amqp from 'amqp-connection-manager'
+import { Channel } from 'amqplib'
 import { getLogger } from '#/utils'
+import { connection } from 'mongoose'
 const { MQ_HOST, MQ_USER, MQ_PASS } = process.env
 
 const logger = getLogger('MQ Client')
+const URL = `amqp://${MQ_USER}:${MQ_PASS}@${MQ_HOST}`
+const CONNECTION = amqp.connect([URL], {
+  json: true,
+  heartbeatIntervalInSeconds: 5,
+  reconnectTimeInSeconds: 10,
+})
+CONNECTION.on('connect', () => {
+  logger('Connected to RabbitMQ server')
+})
+
+CONNECTION.on('disconnect', (params) => {
+  logger.error('RabbitMQ server is disconnected', params.err)
+})
 
 class MQClient {
   static Exchange = 'gps_live'
-  connection: Promise<Connection>
-  channel: Promise<Channel>
-  constructor() {
-    this.connection = connect(
-      {
-        hostname: MQ_HOST,
-        username: MQ_USER,
-        password: MQ_PASS,
-      },
-      {
-        json: true,
-        heartbeatIntervalInSeconds: 5,
-        reconnectTimeInSeconds: 10,
-      },
-    )
 
-    this.channel = this.connection
-      .then((conn) => {
-        return conn.createChannel()
-      })
-      .then((ch) => {
-        ch.assertExchange(MQClient.Exchange, 'topic', { durable: true })
-        return ch
-      })
+  getChannelWrapper(setup: (channel: Channel) => Promise<any>) {
+    return CONNECTION.createChannel({
+      async setup(ch) {
+        await ch.assertExchange(MQClient.Exchange, 'topic', { durable: true })
+        return setup(ch)
+      },
+    })
   }
 
-  async publishGPS(gpsData) {
-    let ch = await this.channel
-    ch.publish(MQClient.Exchange, '', Buffer.from(JSON.stringify(gpsData)))
+  async publishGPS(data) {
+    logger('Publishing GPS data...', data)
+    let channelWrapper = this.getChannelWrapper((ch) => {
+      return ch.assertExchange(MQClient.Exchange, 'topic', { durable: true })
+      ch.publish
+    })
+
+    const startPublishing = () => {
+      return channelWrapper.publish(MQClient.Exchange, '', data)
+    }
+
+    return startPublishing()
   }
 
   ftpConsume(handler) {
     let queue = QUEUE.GPS_UPLOAD
-    ;(async () => {
-      let ch = await this.channel
+    let wrapper = this.getChannelWrapper(async (ch: Channel) => {
       let assertQueue = await ch.assertQueue(queue.name, queue.options)
-      ch.bindQueue(assertQueue.queue, MQClient.Exchange, ''),
-        ch.consume(queue.name, (data) => {
-          try {
-            let message = JSON.parse(data!.content.toString())
-            return handler(message).then(() => {
-              ch.ack(data!)
-              return Promise.resolve(data)
-            })
-          } catch (error) {
-            logger.error(`Got malformed message from queue ${queue.name}`, error, data)
-          }
-        })
-    })()
-  }
-
-  webConsume(token, handler: (data: any) => Promise<any>) {
-    let queue = QUEUE.GPS_LIVE
-    let nameWithToken = queue.name + '-' + token
-    return (async () => {
-      let ch = await this.channel
-      let assertQueue = await ch.assertQueue(nameWithToken, queue.options)
-      ch.bindQueue(assertQueue.queue, MQClient.Exchange, '')
-      let consumer = await ch.consume(nameWithToken, (data) => {
+      await ch.bindQueue(assertQueue.queue, MQClient.Exchange, '')
+      return await ch.consume(queue.name, (data) => {
         try {
           let message = JSON.parse(data!.content.toString())
           return handler(message).then(() => {
@@ -72,18 +58,53 @@ class MQClient {
             return Promise.resolve(data)
           })
         } catch (error) {
-          logger.error(`Got malformed message from queue ${queue.name}`, error, data)
+          logger.error(`Malformed message from queue ${queue.name}`, error, data)
         }
       })
-      return () => {
-        ch.cancel(consumer.consumerTag)
-      }
-    })()
+    })
+    wrapper
+      .waitForConnect()
+      .then(() => {
+        logger(`Consumption from ${queue.name} started!`)
+      })
+      .catch((e) => logger.error('Consumption error', e))
+  }
+
+  webConsume(token, handler: (data: any) => Promise<any>) {
+    let queue = QUEUE.GPS_LIVE
+    let nameWithToken = queue.name + '-' + token
+
+    return new Promise<() => void>((resolve) => {
+      let wrapper = this.getChannelWrapper(async (ch: Channel) => {
+        let assertQueue = await ch.assertQueue(nameWithToken, queue.options)
+        await ch.bindQueue(assertQueue.queue, MQClient.Exchange, '')
+        let consume = await ch.consume(nameWithToken, (data) => {
+          try {
+            let message = JSON.parse(data!.content.toString())
+            return handler(message).then(() => {
+              ch.ack(data!)
+              return Promise.resolve(data)
+            })
+          } catch (error) {
+            logger.error(`Malformed message from queue ${queue.name}`, error, data)
+          }
+        })
+        // return a teardown function for caller
+        resolve(() => ch.cancel(consume.consumerTag))
+      })
+      wrapper
+        .waitForConnect()
+        .then(() => {
+          logger(`Consumption from ${queue.name} started!`)
+        })
+        .catch((e) => logger.error('Consumption error', e))
+    })
   }
 
   async shutdown() {
-    await (await this.channel).close()
-    await (await this.connection).close()
+    connection.close(() => {
+      logger('MQ client shut down')
+    })
   }
 }
 
