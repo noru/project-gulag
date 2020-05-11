7fe55c5cfceb9378c60a1dabfa63752e54f313bb
drew <nooooru@gmail.com>
2020-05-11 22:57:53 +0800

    fix api: param not proper handled

--- packages/server/src/services/rest/routes/gps.ts
+++ packages/server/src/services/rest/routes/gps.ts
@@ -1,25 +1,26 @@
 import Router from 'koa-router'
 import mongoClient from '#/clients/mongo'
 import { responseHelper as error, ErrorCode, ok } from './utils/response'
-import dayjs from 'dayjs'
 
 const model = mongoClient.gps
 
 const router = new Router()
 
 router.get('/api/gps/logs/:imei', async (ctx) => {
-  let { from, to } = ctx.request.query
+
+  let { from = Date.now() - 86400000, to = Date.now() } = ctx.request.query
   let { imei } = ctx.params
 
-  from = +(dayjs(from) || dayjs().subtract(1, 'day'))
-  to = +(dayjs(to) || dayjs())
-
+  if (isNaN(from) || isNaN(to)) {
+    error(ctx, ErrorCode.InvalidParams, new Error('Invalid params, not number'))
+    return
+  }
   if (from > to) {
     error(ctx, ErrorCode.InvalidParams, new Error('Invalid time range'))
     return
   }
 
-  let result = await model.findByIMEI(imei, from, to)
+  let result = await model.findByIMEI(imei, +from, +to)
   ok(ctx, result || [])
 })
 
