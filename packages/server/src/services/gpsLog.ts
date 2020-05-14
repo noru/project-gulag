72b493dbbef4e8ed38f550c4f6ac1f22c66d30fc
noru <nooooru@gmail.com>
2020-05-14 16:58:18 +0800

    delete one month old gps log

--- packages/server/src/services/gpsLog.ts
+++ packages/server/src/services/gpsLog.ts
@@ -2,6 +2,7 @@ import { client } from '#/clients/mq'
 import { LocationMessage } from '#/models/location'
 import { getLogger } from '#/utils'
 import mongoClient from '#/clients/mongo'
+import dayjs from 'dayjs'
 
 const model = mongoClient.gps
 
@@ -13,13 +14,12 @@ function mapper(message: LocationMessage) {
   return {
     imei: message.IMSI,
     timestamp: message.UTC,
-    data: message
+    data: message,
   }
 }
 
 client.gpsLogConsume((data: LocationMessage) => {
-
-  logger('GPS Log incomming', data)
+  logger('GPS Log incoming', data)
   batch.push(data)
   if (batch.length > 1000) {
     logger(`Write ${batch.length} GPS Log to DB`)
@@ -30,3 +30,14 @@ client.gpsLogConsume((data: LocationMessage) => {
   }
   return Promise.resolve()
 })
+
+setInterval(() => {
+  let oneMonthAgo = dayjs().subtract(1, 'month')
+  model.deleteMany({ timestamp: { $lt: oneMonthAgo.valueOf() } }, (err) => {
+    if (err) {
+      logger.error('Error clean gps log', err)
+    } else {
+      logger.info(`Data older than ${oneMonthAgo.toISOString()} cleaned`)
+    }
+  })
+}, 8.64e7 /* one day */)
