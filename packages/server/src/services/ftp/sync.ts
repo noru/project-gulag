ca7b78b4b01d1a46d0fdfcfb4e8c0d063f2e11bd
drew <nooooru@gmail.com>
2020-07-25 11:09:34 +0800

    use cron instead of settimeout

--- packages/server/src/services/ftp/sync.ts
+++ packages/server/src/services/ftp/sync.ts
@@ -1,15 +1,16 @@
 import * as ftp from 'basic-ftp'
 import rimraf from 'rimraf'
 import { getLogger } from '../../utils'
+import { CronJob } from 'cron'
 
 const logger = getLogger('FTP')
-
 const { FTP_HOST, FTP_USER, FTP_PASS, FTP_LOCAL_DIR, FTP_REMOTE_DIR } = process.env
 const client = new ftp.Client()
 client.ftp.log = logger.info
 // client.ftp.verbose = true
 
-async function ftpSync() {
+const ftpSync = new CronJob('*/30 * * * * *', async function () {
+  logger('FTP sync started')
   try {
     await client.access({
       host: FTP_HOST,
@@ -26,8 +27,7 @@ async function ftpSync() {
     logger.error(err)
   } finally {
     client.close()
-    setTimeout(ftpSync, 30000) // 30 sec
   }
-}
+}, null, true, 'Asia/Shanghai');
+ftpSync.start();
 
-ftpSync()
