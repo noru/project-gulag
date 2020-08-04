cffc77a8710f598003f062a59aa13f073a3929ac
drew <nooooru@gmail.com>
2020-08-05 00:16:51 +0800

    fix sftp

--- packages/server/src/services/ftp/sync.ts
+++ packages/server/src/services/ftp/sync.ts
@@ -2,12 +2,15 @@ import rimraf from 'rimraf'
 import { getLogger } from '../../utils'
 import { CronJob } from 'cron'
 import SFTPClient from 'sftp-promises'
+import fs from 'fs'
 
 const sftp = new SFTPClient();
 const logger = getLogger('FTP')
 const { FTP_HOST, FTP_PORT, FTP_USER, FTP_PASS, FTP_LOCAL_DIR, FTP_REMOTE_DIR } = process.env
 
 const ftpSync = new CronJob('*/30 * * * * *', async function () {
+  let list = fs.readdirSync(FTP_LOCAL_DIR!)
+  if (list.length === 0) return
   logger('FTP sync started')
   let session
   try {
@@ -17,15 +20,12 @@ const ftpSync = new CronJob('*/30 * * * * *', async function () {
       user: FTP_USER,
       password: FTP_PASS,
     })
-    let success = await sftp.put(FTP_LOCAL_DIR, FTP_REMOTE_DIR, session)
-    if (success) {
-      logger('File successfully uploaded at ' + new Date())
-      rimraf(FTP_LOCAL_DIR + '/*', () => {
-        logger('Local files cleared')
-      })
-    } else {
-      logger('File upload failed')
+    for (let file of list) {
+      await sftp.put(FTP_LOCAL_DIR + '/' + file, FTP_REMOTE_DIR + '/' + file, session)
     }
+    rimraf(FTP_LOCAL_DIR + '/*', () => {
+      logger('Local files cleared')
+    })
   } catch (err) {
     logger.error(err)
   } finally {
